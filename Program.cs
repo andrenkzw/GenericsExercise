2ce9791ec6cf97b250dc4b635a0d57291f24c623
Andre Nakazawa <andrenkzw@gmail.com>
2023-01-25 17:16:48 -0300

    Treinando Generics

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Generics;
+static class Program
+{
+    public static bool Equals(this int x, int y)
+        => x == y;
+    public static bool Equals(this double x, double y)
+        => x == y;
+    public static bool Equals(this string x, string y)
+        => x == y;
+    public static bool Equals<T>(this T x, T y)
+    {
+        if (x == null)
+        {
+            return y == null;
+        }
+        return x.Equals(y);
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
